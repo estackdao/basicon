ae37e58e8fe3207e17ed3d4abd92f630419e9cac
signalxu <fovzhao@126.com>
2025-06-23 13:04:18 +0200

    improve script and add more metadata

--- basicon/dist/data/chain.mjs
+++
@@ -1,7 +0,0 @@
-import {
-  chain_default
-} from "../chunk-FCUAGMLN.mjs";
-import "../chunk-QK4PKNB3.mjs";
-export {
-  chain_default as default
-};
--- basicon/dist/data/token.mjs
+++
@@ -1,7 +0,0 @@
-import {
-  token_default
-} from "../chunk-PFRTCTDT.mjs";
-import "../chunk-QK4PKNB3.mjs";
-export {
-  token_default as default
-};
